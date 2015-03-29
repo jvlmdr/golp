4e922022161cf4787cebdfd911a711c120ef8636
Jack Valmadre <jack.valmadre@gmail.com>
2015-03-29 14:22:12 -0400

    Remove extraneous NewDict() call in examples

--- lp/ilp_test.go
+++ lp/ilp_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ExampleSolveInt() {
-	dict := lp.NewDict(3, 2)
+	dict := new(lp.Dict)
 	dict.NonBasic = []int{0, 1}
 	dict.Basic = []int{2, 3, 4}
 	// max_{x, y >= 0} x + 2 y
@@ -16,6 +16,8 @@ func ExampleSolveInt() {
 	// -x + y <= 1,     x -  y +  1 >= 0
 	// 3x + 2y <= 12, -3x - 2y + 12 >= 0
 	// 2x + 3y <= 12, -2x - 3y + 12 >= 0
+	dict.A = make([][]float64, 3)
+	dict.B = make([]float64, 3)
 	dict.A[0], dict.B[0] = []float64{1, -1}, 1
 	dict.A[1], dict.B[1] = []float64{-3, -2}, 12
 	dict.A[2], dict.B[2] = []float64{-2, -3}, 12
